3d7574628a890d24de36997b5e9d90c105bb0fde
y-ack <lumage@phytotentillum.localdomain>
2020-03-01 11:16:37 -0800

    customer table

--- customer.h
+++ customer.h
@@ -1,29 +1,27 @@
-
-
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
 #include <stack>
 #include <iostream>
 #include "transaction.h"
-
+#include "item.h"
 
 class Customer {
  public:
-  friend ostream& operator<<(ostream& out, Customer c);
+  friend std::ostream& operator<<(std::ostream& out, Customer c);
   
-  Customer(int id, const string &first_name, const string &last_name);
+  Customer(int id, const std::string &first_name, const std::string &last_name);
 
   void record_transaction(Transaction t);
-  bool borrow_item(Item* item);
-  bool return_item(Item* item)
-  void display_history() const;
+  bool borrow_item(Item* item);  // this probably doesn't go here
+  bool return_item(Item* item);  // since customers don't do anything
+  void display_history() const;  // except keep transaction history
   
  private:
   int id;
-  string first_name;
-  string last_name;
+  std::string first_name;
+  std::string last_name;
   std::stack<Transaction> transaction_history;
-}
+};
 
 #endif // CUSTOMER_H
---
+++ customertable.h
@@ -0,0 +1,36 @@
+#ifndef CUSTOMERTABLE_H
+#define CUSTOMERTABLE_H
+
+#include "customer.h"
+
+// table of Customer data for a Store
+// operations are constant amortized time
+class CustomerTable {
+ public:
+  CustomerTable();
+  ~CustomerTable();
+
+  // add a new customer record to the table
+  void insert(const Customer &c);
+  // remove a customer from the table
+  void remove(int id);
+  // record a customer's Transaction, by customer id
+  void record_transaction(int id, const Transaction& transaction);
+  // retrieve the customer data, by customer id
+  const Customer& retrieve(int id) const;
+
+  // display table entries
+  void display_table() const;
+
+ protected:
+  // calculate hash location for a given id (>=0 & <=9999)
+  //(MOVE THIS TO IMPLEMENTATION:)
+  // using modified (16-bit) Pearson's Method
+  int hash(int id) const;
+
+ private:
+  const static int INITIAL_SIZE = 256;
+  Customer * customers[];
+};
+
+#endif // CUSTOMERTABLE_H
