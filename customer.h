2bcb97a71a1bd53b2fe1647ea987bcb1debe3457
hollowsunsets <isabelgiang@gmail.com>
2020-03-01 19:37:41 -0800

    Remove transaction commands

--- customer.h
+++ customer.h
@@ -13,8 +13,6 @@ class Customer {
   Customer(int id, const std::string &first_name, const std::string &last_name);
 
   void record_transaction(Transaction t);
-  bool borrow_item(Item* item);  // this probably doesn't go here
-  bool return_item(Item* item);  // since customers don't do anything
   void display_history() const;  // except keep transaction history
   
  private:
--- store.h
+++ store.h
@@ -1,4 +1,17 @@
+#include "customertable.h"
+#include "item.h"
+#include <map>
+#include <string>
+
 #ifndef STORE_H
-#define STORE_H
+#define
+class Store {
+public:
+    Store();
+    ~Store();
+private:
+    CustomerTable customer_table;
+    std::map<std::string, Item*> inventory;
+};
 
 #endif // STORE_H
