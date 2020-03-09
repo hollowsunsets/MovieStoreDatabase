bafba640462d1705098056089cfcd32940bff656
hollowsunsets <isabelgiang@gmail.com>
2020-03-08 20:55:26 -0700

    Add misc changes - still broken

--- customertable.cpp
+++ customertable.cpp
@@ -11,85 +11,112 @@
 #include "customertable.h"
 
 CustomerTable::CustomerTable() {
-  customers = new Customer*[INITIAL_SIZE];
+    size = INITIAL_SIZE;
+    elements = 0;
+    load_factor = elements / size;
+    customer_table = new Customer*[INITIAL_SIZE];
 }
 
 CustomerTable::~CustomerTable() {
-  // is this correct i have no idea
-  delete[] customers;
+    for (int i = 0; i < size; i++) {
+        if (customer_table[i] != NULL) {
+            delete customer_table[i];
+        }
+    }
+    delete[] customer_table;
 }
 
 // Quadratic probing, where h(k, i) = (h'(k) + c_1 * i + c_2 * i^2)
-void CustomerTable::insert(const Customer &c) {
-  // TODO: need some way to do table resize
-  // which i totally forgot about; might need fullness member too
-  load_factor = ++elements / size;
-  if (load_factor > 0.5) {
-      // Resize the hash table
-  }
-  int index = hash(c.get_id());
-  // quadratic probing
-  int jumps = 0, m = 0;
-  while (customers[(index + jumps + (jumps * jumps)) % size] != NULL) {
-      ++jumps;
-      m = jumps * jumps;
-  }
-  customers[(index + m) % size] = new Customer(c);
-  load_factor = (++elements / size);
+void CustomerTable::insert(const Customer &c, Customer** customers) {
+    load_factor = ++elements / size;
+    if (load_factor > 0.5) {
+        resize();
+        // Resize the hash table
+    }
+    int index = hash(c.get_id());
+    // quadratic probing
+    int jumps = 0, m = 0;
+    while (customers[(index + m) % size] != NULL) {
+        ++jumps;
+        m = jumps * jumps;
+    }
+    customers[(index + m) % size] = new Customer(c);
+    load_factor = (++elements / size);
 }
 
 
 void CustomerTable::remove(int id) {
-  int index = hash(id);
-  // quadratic probing again; note searching for a match instead of empty
-  int jumps = 0, m = 0;
-  while (customers[(index + m) % size]->get_id() != id) {
-    ++jumps;
-    m = jumps * jumps;
-  }
-  customers[(index + m) % size] = NULL;
+    int index = hash(id);
+    // quadratic probing again; note searching for a match instead of empty
+    int jumps = 0, m = 0;
+    while (customer_table[(index + m) % size]->get_id() != id) {
+        ++jumps;
+        m = jumps * jumps;
+    }
+    customer_table[(index + m) % size] = NULL;
+    elements--;
 }
 
 void CustomerTable::record_transaction(int id, const Transaction &transaction) {
-  // search for id, then call record_transaction on it
-  // use retrieve()
+    // search for id, then call record_transaction on it
+    // use retrieve()
+    Customer* c = this->retrieve(id);
+    // TODO: Assuming retrieve returns NULL if it doesn't find anything.
+    // Probably need to fix this.
+    if (c != NULL) {
+        c->record_transaction(transaction);
+    }
 }
 
 // should this be returning a pointer?
 // if add transaction uses it, can't be const, at least
-Customer& CustomerTable::retrieve(int id) const {
-  // search for id, and return it
-  
-}
-Customer& CustomerTable::operator[](int id) {
-  
-}
+Customer* CustomerTable::retrieve(int id) const {
+    int index = hash(id);
+    int jumps = 0, m = 0;
 
+    while (customer_table[(index + m) % size]->get_id() != id) {
+        ++jumps;
+        m = jumps * jumps;
+    }
+    // TODO: What if the key doesn't exist in the hash table?
+    return customer_table[(index + m) % size];
+}
 
 void CustomerTable::display_table() const {
-  // just loop through and print indexes + value
+    // just loop through and print indexes + value
 }
 
 // Pearson hashing modified to yield up to 16 bits
 // we expect low entropy in the high bits of id
 int CustomerTable::hash(int id) const {
-  // initialize high and low parts
-  // considering using uint_8 here
-  int hash_lo = shuffle[id & 0x00FF];
-  int hash_hi = shuffle[(id & 0xFF00) >> 8];
-  // shuffle round 1 based on xor (low against low)
-  hash_lo = shuffle[hash_lo ^ (id & 0x00FF)];
-  // shuffle round 2 based on xor (shuffled low against high)
-  hash_lo = shuffle[hash_lo ^ (id & 0xFF00) >> 8];
+    // initialize high and low parts
+    // considering using uint_8 here
+    int hash_lo = SHUFFLE[id & 0x00FF];
+    int hash_hi = SHUFFLE[(id & 0xFF00) >> 8];
+    // shuffle round 1 based on xor (low against low)
+    hash_lo = SHUFFLE[hash_lo ^ (id & 0x00FF)];
+    // shuffle round 2 based on xor (shuffled low against high)
+    hash_lo = SHUFFLE[hash_lo ^ (id & 0xFF00) >> 8];
 
-  // high against low/high
-  hash_hi = shuffle[hash_hi ^ (id & 0x00FF)];
-  hash_hi = shuffle[hash_hi ^ (id & 0xFF00) >> 8];
+    // high against low/high
+    hash_hi = SHUFFLE[hash_hi ^ (id & 0x00FF)];
+    hash_hi = SHUFFLE[hash_hi ^ (id & 0xFF00) >> 8];
 
-  // combine to get 16 bits, then modulo size  
-  return ((hash_hi << 8) | hash_lo) % size;
+    // combine to get 16 bits, then modulo size
+    return ((hash_hi << 8) | hash_lo) % size;
 }
 
-void CustomerTable::resize(int capacity) {
-    
+void CustomerTable::resize() {
+    Customer** new_customer_table = new Customer*[size << 1];
+    this->elements = 0;
+    for (int i = 0; i < size; i++) {
+        if (customer_table[i] != NULL) {
+            this->insert(customer_table[i], new_customer_table);
+            // TODO: Not sure if this even works
+            delete customer_table[i];
+        }
+    }
+    delete[] customer_table;
+    this->customer_table = new_customer_table;
+    this->size = size << 1;
 }
